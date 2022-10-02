a276239d37c07f093f636a3b335a2aca1772d84c
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-10-02 13:23:13 -0400

    9/29 class

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!!');
-});
+    res.end('Hello World!!!!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 // call back function
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    console.log('Server still running at http://' + hostname + ':' + port +'/');
 });
 
 
