08f90303923730194550fa63fb4e83fc8194d40c
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-10-13 14:14:57 -0400

    class 10/3

--- server/index.js
+++ server/index.js
@@ -1,32 +1,18 @@
-const express = require('express')
-const app = express()
+const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!!!!');
-})
+const server = http.createServer((req, res) => {
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Happy New Year');
 
-// call back function
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
-
-/**
-const nnn = function(parnmName){
-}
-const obj = {
-    vvv(){
-    }
-}
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log('Server still running at http://' + hostname + ':' + port + '/');
+});
 const fatArrow = x => x * 2;
-*/
