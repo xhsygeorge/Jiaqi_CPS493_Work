469b993848d8b6f863a5afbf93d3ba5f7e900116
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-10-27 15:06:00 -0400

    10/27

---
+++ client/src/components/cart.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+    
+</script>
+<template>
+    <div class="fly-out">
+
+    </div>
+</template>
+
+
+
+<style scoped>
+    .fly-out{
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        width: 20em;
+        background-color: #ffffffaa;
+        border-left: 100;
+    }
+</style>
