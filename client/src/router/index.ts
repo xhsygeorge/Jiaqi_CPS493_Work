752a8ca60473555eb1d70cd1b5c0c11fb931954c
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-10-27 14:13:27 -0400

    10/24

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,63 +1,31 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import StatisticsView from "../views/StatisticsView.vue";
-import MyActivityView from "../views/MyActivityView.vue";
-import FriendsActivityView from "../views/FriendsActivityView.vue";
-import PeopleSearchView from "../views/PeopleSearchView.vue";
-import User from "../views/UserView.vue";
-import ProductsViewVue from "@/views/ProductsView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductsView from '../views/ProductsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      //没有import AboutView from "../views/AboutView.vue"
-      //用法和其他的不一样
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/myactivity",
-      name: "myactivity",
-      component: MyActivityView,
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
     },
     {
-      path: "/statistics",
-      name: "statistics",
-      component: StatisticsView,
+        path: '/products',
+        name: 'products',
+        component: ProductsView,
     },
     {
-      path: "/friendsactivity",
-      name: "friendsactivity",
-      component: FriendsActivityView,
+        path: '/product/:id',
+        name: 'product',
+        component: () => import('../views/ProductDetailsView.vue'),
     },
-    {
-      path: "/peoplesearch",
-      name: "peoplesearch",
-      component: PeopleSearchView,
-    },
-    {
-      path: "/user",
-      name: "user",
-      component: User,
-    },
-    {
-      path: "/products",
-      name: "products",
-      component: ProductsView,
-    },
-    {
-      path: "/product/:id",
-      name: Product,
-      component: ProductDetailsView,
-    },
-  ],
-});
+  ]
+})
 
 export default router;
--- client/src/views/ProductDetailsView.vue
+++ client/src/views/ProductDetailsView.vue
@@ -1,51 +1,41 @@
-<script lang="ts">
-import { reactive } from 'vue';
-import { getProducts } from "../stores/products";
-import { RouterLink } from 'vue-router';
-import { ref } from "vue";
+<script setup lang="ts">
+    import { ref } from "vue";
+    import { useRoute } from "vue-router";
 
-const route = useRoute();
-route.param.id
+    import { getProduct } from "../stores/products";
 
-const product = ref(getProducts(+route.params.id));
+    const route = useRoute();
+    
+    const product = ref(getProduct(+route.params.id));
+
 </script>
 
 <template>
-    <div>
-        <div v-if="products" class="section">
-            <div class="product-image">
-                <img src="product.thumbnail" alt="product.title" />
-            </div>
-            <div class="product-info">
-                <h3 class="title">{{ product.title}}</h3>
-                <span class="currency">$</span>
-                <span class="amount">{{ product.price}}</span>
-            </div>
+    <div v-if="product" class="section">
+        <div class="product-image">
+            <img :src="product.thumbnail" :alt="product.title" />
         </div>
+        <div class="product-info">
+            <h3 class="title">{{ product.title }}</h3>
+            <p class="price subtitle">
+                <span class="currency">$</span>
+                <span class="amount">{{ product.price }}</span>
+            </p>
+            <p>{{ product.description }}</p>
+
+        </div>
+    </div>
+    <div v-else class="section">
+        <h2 class="title">Product not found</h2>
     </div>
 </template>
 
-
-
 <style scoped>
-  .products{
-    display: flex;
-    flex-wrap: wrap;
-  },
-  .product{
-    flex-basis: 10em;
-    margin: 1em;
-    padding: 1em;
-    border: 1em solid #000;
-    border-radius: 5em;
-    background-color: #fff;
-  },
-  .product-info{
-    display: flex;
-    align-items: flex-start;
-
-  }
-  .price{
-
-  }
+    .section {
+        display: flex;
+        background-color: aliceblue;
+    }
+    .product-info {
+        padding: 1em;;
+    }
 </style>
