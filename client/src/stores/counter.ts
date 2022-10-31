23e5bb97da7f33ea195500d07eb175408d7e8366
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-10-31 13:34:19 -0400

    10/27

--- client/src/stores/counter.ts
+++ client/src/stores/counter.ts
@@ -1,12 +1,33 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { reactive } from "vue";
+import type { Product } from "./products";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export interface CartItem {
+    quantity: number;
+    product: Product;
+}
 
-  return { count, doubleCount, increment }
-})
+const cart = reactive([] as CartItem[]);
+
+export function addProductToCart(product: Product, quantity: number = 1) {
+    const cartItem = cart.find((item) => item.product.id === product.id);
+    if (cartItem) {
+        cartItem.quantity += quantity;
+    } else {
+        cart.push({
+            quantity,
+            product,
+        });
+    }
+}
+
+export function updateProductQuantity(id: number, quantity: number) {
+    const cartItem = cart.find((item) => item.product.id === id);
+    if (cartItem) {
+        cartItem.quantity = quantity;
+        if (cartItem.quantity <= 0) {
+            cart.splice(cart.indexOf(cartItem), 1);
+        }
+    }
+}
+
+export default cart;
