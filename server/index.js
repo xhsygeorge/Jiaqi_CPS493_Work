0f7bddc7293ba5aa3531c79a66812663fbf0e3cf
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-11-01 19:44:05 -0400

    10/31 class

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+
+const productsController = require('./controllers/products');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+app
+.get('/', (req, res) => {
+    res.status(200).send('Happy Sweet New Year');
+})
+.use('/api/v1/products', productsController)
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Happy New Year');
 
-});
 
-server.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    console.log('Server still running at http://' + hostname + ':' + port + '/');
 });
-const fatArrow = x => x * 2;
