7b371838cd54e01b8d930e800c23d8d3a57c0eb6
GeorgeChen <72675959+xhsygeorge@users.noreply.github.com>
2022-12-09 16:40:58 -0500

    12/8 class
    next-app and react -app

---
+++ next-app/pages/_app.tsx
@@ -0,0 +1,23 @@
+import 'bulma'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import NavVue from '../components/Nav'
+
+
+//const NavVue = () => <div className="navbar is-primary" role="navigation" aria-label="main navigation"></div>
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="root">
+      <header>
+        <NavVue />
+      </header>
+
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </div>
+    
+  )
+}
